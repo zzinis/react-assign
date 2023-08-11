8ee279a23165a4a315691c9fe3aac41d559f3b17
zzinis <lizzyin@naver.com>
2023-08-11 20:24:55 +0900

    Update App.js

--- assign/src/App.js
+++ assign/src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import App01 from './pages/App01';
 import App06 from './pages/App06';
-
+import Todo from './pages/Todo';
 
 function App() {
   return (
     <>
       {/* <App01 /> */}
-      <App06 />
+      {/* <App06 /> */}
+      <Todo />
     </>
   );
 }
